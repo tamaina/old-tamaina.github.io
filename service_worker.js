ad4411bd3a99826718937819b20a471d127d4da9
tamaina <tamaina@hotmail.co.jp>
2018-12-25 04:20:27 +0900

    :v:

---
+++ service_worker.js
@@ -0,0 +1,8 @@
+/* workbox 2018-12-24T19:18:49.374Z */
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+workbox.routing.registerRoute(
+    /.*.(?:js|css|png|jpeg|jpg|svg|svgz|woff2)/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'assets-cache',
+    })
+);
