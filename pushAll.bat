0a1b447ac2f691414ea9c8966efe9a6c0fbf4715
tamaina <tamaina@hotmail.co.jp>
2018-12-25 03:53:47 +0900

    :v:

--- pushAll.bat
+++ pushAll.bat
@@ -2,11 +2,11 @@ git fetch
 cd pages
 git add --all
 git commit -a -m %1
-git push
+git push -u origin pages
 cd ../docs
 git add --all
 git commit -a -m %1
-git push
+git push -u origin pages
 cd ..
 git add --all
 git commit -a -m %1
