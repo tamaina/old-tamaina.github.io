24ffa3a566555ec9389fb7a4e85bcc75889b946f
tamaina <tamaina@hotmail.co.jp>
2018-12-26 13:51:09 +0900

    fix

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-/* workbox 2018-12-26T04:35:16.561Z */
+/* workbox 2018-12-26T04:50:35.721Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 workbox.routing.registerRoute(
     /.*.(?:js|css|png|jpeg|jpg|svg|svgz|woff2)/,
