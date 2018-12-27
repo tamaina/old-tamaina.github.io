56410c9bd5c4bc6f43414ab029d31000de53d880
tamaina <tamaina@hotmail.co.jp>
2018-12-28 02:19:53 +0900

    :v:

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-/* workbox 2018-12-27T17:16:57.605Z */
+/* workbox 2018-12-27T17:19:16.457Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 workbox.routing.registerRoute(
     /.*.(?:js|css|png|jpeg|jpg|svg|svgz|woff2)/,
@@ -9,7 +9,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute([
     {
         url: '/offline/',
-        revision: '1545931017605',
+        revision: '1545931156457',
     }
 ]);
 self.addEventListener('fetch', function(event) {
