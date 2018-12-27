03d77abdf4b90c4733443dd9259d6b740d352d11
tamaina <tamaina@hotmail.co.jp>
2018-12-28 01:36:25 +0900

    :v:

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-/* workbox 2018-12-26T05:19:44.974Z */
+/* workbox 2018-12-27T16:35:34.835Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 workbox.routing.registerRoute(
     /.*.(?:js|css|png|jpeg|jpg|svg|svgz|woff2)/,
@@ -6,3 +6,20 @@ workbox.routing.registerRoute(
         cacheName: 'assets-cache',
     })
 );
+workbox.precaching.precacheAndRoute([
+    {
+        url: '/offline/',
+        revision: '1545928534835',
+    }
+]);
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            return response || fetch(event.request);
+        })
+        .catch(function() {
+            return caches.match('/offline/');
+        })
+    );
+});
