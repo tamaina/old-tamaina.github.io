6853ea7760b5aa4e9f20f71c712a681c45110ffc
tamaina <tamaina@hotmail.co.jp>
2018-12-29 08:09:55 +0900

    fix

--- theme/js/sw.ts
+++ theme/js/sw.ts
@@ -11,14 +11,18 @@ export const sw = (): void => {
             navigator.serviceWorker.register(window.jm_pathToWorker)
             .then(registration => {
                 console.log('Service Worker: 登録: ', registration.scope)
+                registration.onupdatefound = () => {
+                    if (registration.installing) {
+                        registration.installing.onstatechange = () => {
+                            if(registration.installing.state == 'installed' && navigator.serviceWorker.controller){
+                                console.log('Service Worker: バージョンアップします...')
+                                location.reload(true)
+                            }
+                        }
+                    }
+                }
             }).catch(err => {
-                console.log('Service Worker: 登録時にエラーが発生しました: ', err)
-            })
-
-            navigator.serviceWorker.ready
-            .then(() => location.reload(true))
-            .catch(err => {
-                console.log('error on navigator.serviceWorker.ready', err)
+                console.log('Service Worker: 登録時にエラー発生しました: ', err)
             })
         }
     }
