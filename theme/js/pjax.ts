a70328bb85feb62c56b45a17e1eb47da7b75902f
tamaina <tamaina@hotmail.co.jp>
2019-01-13 00:56:10 +0900

    fix

--- theme/js/pjax.ts
+++ theme/js/pjax.ts
@@ -2,7 +2,7 @@ export const pjaxinit = async (): Promise<void> => {
     const { Pjax } = await import('pjax-api')
     new Pjax({
         areas: [
-            '#main, #breadcrumb, #mainnav, #updateTime, #sidebar',
+            '#main, #breadcrumb, #mainnav, #updateTime, #sidebar, #footer',
             '#grid',
             'body'
         ],
