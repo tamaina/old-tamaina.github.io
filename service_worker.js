3d58964ac5a1661353514fda7b1c41a99ec104c1
tamaina <tamaina@hotmail.co.jp>
2019-02-09 03:24:23 +0900

    Lint

--- service_worker.js
+++ service_worker.js
@@ -1,31 +1,31 @@
-/* workbox 2019-02-08T08:29:10.132Z */
+/* workbox 2019-02-08T18:17:25.681Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.routing.registerRoute(
     /.*.(?:js|css|png|jpeg|jpg|svg|svgz|woff2)/,
     workbox.strategies.staleWhileRevalidate({
-        cacheName: 'assets-cache',
+        cacheName: "assets-cache",
     })
 );
 workbox.precaching.precacheAndRoute([
     {
-        url: '/offline/',
-        revision: '1549614550132',
+        url: "/offline/",
+        revision: "1549649845681",
     }
 ]);
 workbox.skipWaiting();
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function(event) {
     event.respondWith(
         caches.match(event.request)
         .then(function(response) {
             return response || fetch(event.request);
         })
         .catch(function() {
-            return caches.match('/offline/');
+            return caches.match("/offline/");
         })
     );
 });
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function(event) {
     workbox.skipWaiting();
     workbox.clientsClaim();
 })
