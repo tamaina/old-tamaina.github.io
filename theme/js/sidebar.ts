11909d8ac96411bc7c62c160fe4f1337300fb933
tamaina <tamaina@hotmail.co.jp>
2019-08-26 12:46:38 +0900

    :v:

--- theme/js/sidebar.ts
+++ theme/js/sidebar.ts
@@ -94,9 +94,9 @@ export class Sidebar {
   private touchmove(e: TouchEvent) {
     this.diffX = e.changedTouches[0].pageX - this.startX
     if (this.diffX >= 120) {
-      if (this.startX < 20 && !this.opened) this.sidebarOpen()
+      if (this.startX < 5 && !this.opened) this.sidebarOpen()
     } else if (this.diffX > 0) {
-      if (this.startX < 20 && !this.opened) {
+      if (this.startX < 5 && !this.opened) {
         document.body.style.overflowY = "hidden"
         for (const target of this.targets) target.style.left = `calc(-70vw + ${this.diffX}px)`
       }
