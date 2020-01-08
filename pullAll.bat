7b799d07737437a9976be9b807c95ad8172762df
tamaina <tamaina@hotmail.co.jp>
2020-01-08 11:07:17 +0900

    :v:

--- pullAll.bat
+++ pullAll.bat
@@ -2,13 +2,13 @@ git fetch
 
 git add --all
 git commit -a -m %1
-git pull -u origin pages
+git pull origin pages
 cd pages
 git add --all
 git commit -a -m %1
-git pull -u origin pages
+git pull origin pages
 cd ../docs
 git add --all
 git commit -a -m %1
-git pull -u origin master
+git pull origin master
 cd ..
