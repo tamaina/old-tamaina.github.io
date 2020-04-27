dc06bc96b3816ad192677ed3e0452f5c223c3d4b
tamaina <tamaina@hotmail.co.jp>
2020-04-27 23:00:54 +0900

    %1

---
+++ pullAll.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+git fetch
+
+git add --all
+git commit -a -m %1
+git pull origin src
+cd pages
+git add --all
+git commit -a -m %1
+git pull origin pages
+cd ../docs
+git add --all
+git commit -a -m %1
+git pull origin master
+cd ..
