530853bd29812e0baed1c20c8cfeeb2015f04a5d
tamaina <tamaina@hotmail.co.jp>
2020-05-05 19:28:05 +0900

    %1

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-/* workbox 2020-05-05T10:06:59.569Z */
+/* workbox 2020-05-05T10:27:05.589Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.skipWaiting();
@@ -51,7 +51,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute([
     {
         url: "/offline",
-        revision: "1588673219569",
+        revision: "1588674425589",
     }
 ]);
 
