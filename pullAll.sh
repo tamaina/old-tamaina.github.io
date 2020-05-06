ffac701d8894dad111703090a6e21d544a770c6a
tamaina <tamaina@hotmail.co.jp>
2020-05-06 20:18:03 +0900

    hikisuu

--- pullAll.sh
+++ pullAll.sh
@@ -2,14 +2,14 @@
 git fetch
 
 git add --all
-git commit -a -m %1
+git commit -a -m $1
 git pull origin src
 cd pages
 git add --all
-git commit -a -m %1
+git commit -a -m $1
 git pull origin pages
 cd ../docs
 git add --all
-git commit -a -m %1
+git commit -a -m $1
 git pull origin master
 cd ..
--- pushAll.sh
+++ pushAll.sh
@@ -2,14 +2,14 @@
 git fetch
 
 git add --all
-git commit -a -m %1
+git commit -a -m $1
 git push -u origin src
 cd pages
 git add --all
-git commit -a -m %1
+git commit -a -m $1
 git push -u origin pages
 cd ../docs
 git add --all
-git commit -a -m %1
+git commit -a -m $1
 git push --force-with-lease origin master
 cd ..
