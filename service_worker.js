578dc37ff000eebe41e9e12bca5a152cef327e32
tamaina <tamaina@hotmail.co.jp>
2021-03-02 16:54:49 +0900

    :v:

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,4 @@
-/* workbox 2020-05-05T10:27:05.589Z */
+/* workbox 2021-03-02T07:53:52.264Z */
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.skipWaiting();
@@ -51,7 +51,7 @@ workbox.routing.registerRoute(
 workbox.precaching.precacheAndRoute([
     {
         url: "/offline",
-        revision: "1588674425589",
+        revision: "1614671632264",
     }
 ]);
 
